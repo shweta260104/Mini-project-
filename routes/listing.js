fcad0107fb3a295fbd71c9598ea28d6de5043b1c
shweta260104 <jadhav26012004@gmail.com>
2024-11-27 15:13:17 +0530

    Update listing.js

--- routes/listing.js
+++ routes/listing.js
@@ -10,7 +10,7 @@ const {isLoggedIn, isOwner} = require("../middleware.js");
 const listingController = require("../controllers/listing.js");
 
 // Index Route
-router.get("/", wrapAsync (index) );
+router.get("/", wrapAsync(listingController.index));
     
 
 // New Route
@@ -42,7 +42,7 @@ router.get("/:id",
 router.post(
     "/",
     isLoggedIn,
-    validateListing,
+   
     wrapAsync(async (req, res, next) => {
         const newListing = new Listing(req.body.listing);
         newListing.owner =req.user._id;
@@ -73,7 +73,6 @@ router.put(
     "/:id",
     isLoggedIn,
     isOwner,
-    validateListing,
     wrapAsync(async (req, res) => {
         let {id} = req.params;
          await Listing.findByIdAndUpdate(id, { ...req.body.listing });
@@ -98,3 +97,6 @@ router.delete("/:id", isLoggedIn,isOwner,wrapAsync(async (req, res) => {
 
 
 module.exports = router;
+    
+        
+        
